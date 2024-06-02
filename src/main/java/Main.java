70c5bb7f5fb867453dba2babfc48b327805c7b61
jadiel07 <125308414+jadiel07@users.noreply.github.com>
2024-06-02 19:09:37 -0300

    Delete src/main/java directory

--- src/main/java/Main.java
+++
@@ -1,8 +0,0 @@
-package main.java;
-
-public class Main {
-    public static void main(String[] args) {
-        InitialScreen initialScreen = new InitialScreen();
-        initialScreen.setVisible(true);
-    }
-}
